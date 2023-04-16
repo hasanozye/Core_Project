c156d0ddfccf78c97dcb0d494d5a958376948758
Hasan Ozyer <hasanlareli@gmail.com>
2023-04-16 14:24:12 +0300

    Core project has been created

---
+++ src/day07/MainApp.java
@@ -0,0 +1,12 @@
+package day07;
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("ahahhahahahaha");
+        topla();
+    }
+
+    public static void topla(){
+        System.out.println(3+4);
+    }
+}
---
+++ src/day07/Variables.java
@@ -0,0 +1,14 @@
+package day07;
+
+public class Variables {
+    public static void main(String[] args) {
+        // = Operant (assignment), sağında bulunan bir değer (Literal) veya bir işlemin sonucunu
+        // solundaki değişkenin (Variable) temsil ettiği hafiza bölgesine kaydeder.
+        int age = 34;  //bu satır bir java ifadesidir(Statement)
+
+        //Karenin alanını ve çevresini hesaplayınız.
+        int kenar = 5;
+        System.out.println(kenar*kenar);
+        System.out.println(kenar*4);
+    }
+}
