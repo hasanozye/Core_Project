76ea323c01602fea48233931edf938031533d405
Hasan Ozyer <hasanlareli@gmail.com>
2023-04-16 14:25:43 +0300

    Core project has been modified

--- src/day07/Variables.java
+++ src/day07/Variables.java
@@ -4,7 +4,7 @@ public class Variables {
     public static void main(String[] args) {
         // = Operant (assignment), sağında bulunan bir değer (Literal) veya bir işlemin sonucunu
         // solundaki değişkenin (Variable) temsil ettiği hafiza bölgesine kaydeder.
-        int age = 34;  //bu satır bir java ifadesidir(Statement)
+        //bu satır bir java ifadesidir(Statement)
 
         //Karenin alanını ve çevresini hesaplayınız.
         int kenar = 5;
