3259d46eff7a7c7d18d910fcef1c79b1cbfad71c
Hasan Ozyer <hasanlareli@gmail.com>
2023-04-20 17:09:29 +0300

    Summary of variables exercises

---
+++ src/day08/Summary.java
@@ -0,0 +1,27 @@
+package day08;
+
+import java.util.Arrays;
+
+public class Summary {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        //CPU
+        int sayi = 532412359;
+        System.out.println("sayi = " + sayi);
+
+        boolean result = true;
+        System.out.println("result = " + result);
+        boolean result2 = false;
+        System.out.println("result2 = " + result2);
+        boolean result3 = 6>3;
+        System.out.println("result3 = " + result3);
+        System.out.println("Summary.main");
+        System.out.println("args = " + Arrays.toString(args));
+        String str = "23"+15;
+        System.out.println("str = " + str);
+
+        String mesaj ="Ççular, \n hep bir ağı\tzd\\>an\b";
+        System.out.println("mesaj = " + mesaj);
+
+    }
+}
---
+++ src/day08/Task6.java
@@ -0,0 +1,28 @@
+package day08;
+
+public class Task6 {
+    public static void main(String[] args) {
+        int flightNum = 10;
+        System.out.println("flightNum = " + flightNum);
+
+        int variable = 29;
+        System.out.println("variable = " + variable);
+
+        String str = "Java programming";
+        System.out.println("str = " + str);
+
+        float floatValue = 12.5F;
+        System.out.println("floatValue = " + floatValue);
+
+//        TASk-8
+        int year= 1491;
+        String message = "IN " + year +" COLUMBUS SAILED THE OCEANBLUE";
+        System.out.println("message = " + message);
+        year = 1492;
+        message = "IN " + year + " COLUMBUS SAILED THE OCEANBLUE";
+        System.out.println("Edited Message = " + message);
+
+
+
+    }
+}
