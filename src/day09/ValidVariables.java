a9527e82baaae598dc3fded8cc11039500f18a70
Hasan Ozyer <hasanlareli@gmail.com>
2023-04-20 18:48:19 +0300

    Summary of variables exercises-2

---
+++ src/day09/ValidVariables.java
@@ -0,0 +1,10 @@
+package day09;
+
+public class ValidVariables {
+    public static void main(String[] args) {
+        String xet = "Gürhan";
+        System.out.println("xet = " + xet);
+        int sayi = 10;
+        System.out.println("sayi = " + sayi);
+    }
+}
