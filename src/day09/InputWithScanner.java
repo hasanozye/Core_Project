964800e1ff2519c97a03168d3d2ab3c187dddae3
Hasan Özyer <hasanozyer06@gmail.com>
2023-04-21 15:20:40 +0300

    Scanner Class Practises

--- src/day09/InputWithScanner.java
+++ src/day09/InputWithScanner.java
@@ -8,12 +8,24 @@ public class InputWithScanner {
         Scanner klavyeGirdisi = new Scanner(System.in);
         System.out.print("Birinci Sayı : ");
         int sayi1 = klavyeGirdisi.nextInt();
+        klavyeGirdisi.nextLine();
 
         System.out.print("İkinci Sayı : ");
         int sayi2 = klavyeGirdisi.nextInt();
 
         int toplam = sayi2 + sayi1;
         System.out.println("Girilen sayıların toplamı = " + toplam);
+
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+
+        System.out.println("String: " +s);
+        System.out.println("Double: " +d);
+        System.out.println("Int: " +i);
+        scan.close();
 
     }
 }
