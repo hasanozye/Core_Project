e2518aaeda56a469b13c44d11af59b533e057856
Hasan Özyer <hasanozyer06@gmail.com>
2023-04-24 00:52:03 +0300

    if statement

---
+++ src/day14/IfStatement.java
@@ -0,0 +1,22 @@
+package day14;
+
+public class IfStatement {
+    public static void main(String[] args) {
+        /*
+        Akış (Flow ) kontrol ifadeleri
+        if      :
+        switch  :
+        for     :
+        while   :
+         */
+        int x = 0;
+        int y = 10;
+        if (y == 20) {
+            x = 5;
+
+        }
+        System.out.println("x = " + x);
+
+
+    }
+}
