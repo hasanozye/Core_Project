ab437852c570177900aebebd45b4c7b23685cc7b
Hasan Özyer <hasanozyer06@gmail.com>
2023-04-25 22:59:49 +0300

    switch task practice

---
+++ src/day15/MenuTasarimi.java
@@ -0,0 +1,8 @@
+package day15;
+
+public class MenuTasarimi {
+    public static void main(String[] args) {
+        //TODO Devam edilcek
+
+    }
+}
---
+++ src/day15/SwitchKullanimi.java
@@ -0,0 +1,36 @@
+package day15;
+
+public class SwitchKullanimi {
+    public static void main(String[] args) {
+        int gun = 8;
+        switch (gun) {
+            case 1:
+                System.out.println("Pazartesi");
+                break;
+            case 2:
+                System.out.println("Salı");
+                break;
+            case 3:
+                System.out.println("Çarşamba");
+                break;
+            case 4:
+                System.out.println("Perşembe");
+                break;
+            case 5:
+                System.out.println("Cuma");
+                break;
+            case 6:
+                System.out.println("Cumartesi");
+                break;
+            case 7:
+                System.out.println("Pazar");
+                break;
+            default:
+                System.out.println("Yanlış seçim.");
+
+
+        }
+        //break imleci buraya atar
+        System.out.println("Exited from switch.");
+    }
+}
