fefa2a6acf45df47af9e2ea5ae8c1f737231461d
Hasan Özyer <hasanozyer06@gmail.com>
2023-04-29 00:35:38 +0300

    case practice

--- src/day15/MenuTasarimi.java
+++ src/day15/MenuTasarimi.java
@@ -1,8 +1,36 @@
 package day15;
 
+import java.util.Scanner;
+
 public class MenuTasarimi {
     public static void main(String[] args) {
         //TODO Devam edilcek
-
+//        Dört işlem yapabilen Hesap Makinesi Tasarlayınız.
+        System.out.println("-".repeat(20));
+        System.out.println("     Dört İşlem     ");
+        System.out.println("1. Toplama");
+        System.out.println("2. Toplama");
+        System.out.println("3. Toplama");
+        System.out.println("4. Toplama");
+        System.out.println("-".repeat(20));
+        System.out.print("Seçiminiz: ");
+        Scanner scanner = new Scanner(System.in);
+        int secim = scanner.nextInt();
+        System.out.println(secim);
+        switch (secim){
+            case 1:
+                System.out.println("Toplama");
+                break;
+            case 2:
+                System.out.println("Çıkarma");
+                break;
+            case 3:
+                System.out.println("Çarpma");
+                break;
+            case 4:
+                System.out.println("Bölme");
+                break;
+            default: break;
+        }
     }
 }
