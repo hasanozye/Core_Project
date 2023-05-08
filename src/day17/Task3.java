a9a10ac28adc53c1cb94c965051f9d943c3be243
Hasan Özyer <hasanozyer06@gmail.com>
2023-05-08 18:02:02 +0300

    fixed a problem that existed in for loop

--- src/day17/Task3.java
+++ src/day17/Task3.java
@@ -2,17 +2,15 @@ package day17;
 
 public class Task3 {
     public static void main(String[] args) {
-        double kph = 1;
-        double mph = kph * 0.6214;
-        double random = 60;
-        //TODO: BU PROBLEM ÇÖZÜLCEK!
+
+        //update: problem çözüldü...
 
         System.out.println("KPH\t\tMPH");
+
         for (int i = 60; i <= 130; i += 10) {
-            System.out.println(i + "\t");
-            kph = random;
-            System.out.printf("%.1f",mph);
-            random+=10;
+            System.out.print(i + "\t\t");
+            double mph = i * 0.6214;
+            System.out.printf("%4.1f\n", mph);
 
         }
     }
