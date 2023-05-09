8594bed85811db01e1b6068ebc7ee7918e16845d
Hasan Özyer <hasanozyer06@gmail.com>
2023-05-09 21:45:55 +0300

    Array problem practice

---
+++ src/day19/TaskAboutArray.java
@@ -0,0 +1,22 @@
+
+package day19;
+
+public class TaskAboutArray {
+    public static void main(String[] args) {
+        /*
+        * Store grades of twelve students in an integer array variable. Write a Java program that finds the average of these grades.
+
+grades: 56, 75, 90, 47, 83, 58, 33, 86, 92, 65, 90, 45
+
+average?*/
+        int[] grades = {56, 75, 90, 47, 83, 58, 33, 86, 92, 65, 90, 45};
+        int sum = 0;
+        for (int i = 0; i < grades.length; i++) {
+            sum += grades[i];
+        }
+        double average = sum / 12.0;
+        System.out.printf("Average = %.2f", average);
+
+
+    }
+}
