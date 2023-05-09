4e719e39c44923c91b3dcd03b829e76acb7097ac
Hasan Özyer <hasanozyer06@gmail.com>
2023-05-09 19:01:43 +0300

    Dummy Board for my practise

---
+++ src/Homeworks/day16/Tasks.java
@@ -0,0 +1,11 @@
+package Homeworks.day16;
+
+public class Tasks {
+    public static void main(String[] args) {
+
+        String S = "21312";
+        int N = 23;
+        System.out.printf("The input string:%s\n",S);
+        System.out.printf("The input integer:%d",N);
+    }
+}
