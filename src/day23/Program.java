ce385426e69b1f2adabf9b20ac48b0630f562d35
Hasan Özyer <hasanozyer06@gmail.com>
2023-05-16 09:12:12 +0300

    OOP Dog example

---
+++ src/day23/Program.java
@@ -0,0 +1,10 @@
+package day23;
+
+import day24.Kopek;
+
+public class Program {
+    public static void main(String[] args) {
+        Kopek k1 = new Kopek();
+
+    }
+}
---
+++ src/day24/Kopek.java
@@ -0,0 +1,13 @@
+package day24;
+
+public class Kopek {
+    public String cinsi;
+    public int yasi;
+    public double agirligi;
+
+    void bilgiGoster() {
+
+        System.out.printf("%s köpeği [%d - %.1f Kg.]", cinsi, yasi, agirligi);
+    }
+
+}
