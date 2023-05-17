4e401540ad72838c1f1d4ef86db7794871e8232b
Hasan Özyer <hasanozyer06@gmail.com>
2023-05-17 08:50:06 +0300

    OOP Lion and Cat practice

---
+++ src/day25/hayvanlar/Aslan.java
@@ -0,0 +1,9 @@
+package day25.hayvanlar;
+
+public class Aslan {
+    public double boy;
+    public double kilo;
+    public String turu;
+    public String cinsi;
+    public String rengi;
+}
---
+++ src/day25/hayvanlar/Kedi.java
@@ -0,0 +1,16 @@
+package day25.hayvanlar;
+
+public class Kedi {
+    public String cinsi;
+    public int yasi;
+    public double agirligi;
+
+
+    public Kedi(String c, int y, double a){
+        cinsi = c;
+        yasi = y;
+        agirligi = a;
+    }
+
+
+}
