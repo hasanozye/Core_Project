da7b6261f81a2b1a12dfa8cb8911a1d0fbeddcea
Hasan Özyer <hasanozyer06@gmail.com>
2023-05-18 20:44:24 +0300

    OOP principles and examples

---
+++ src/day26/Program.java
@@ -0,0 +1,22 @@
+package day26;
+
+import day26.math.DortIslem;
+
+public class Program {
+    public static void main(String[] args) {
+        DortIslem islem1 = new DortIslem(5,7);
+
+        System.out.println("Toplam = "+islem1.topla());
+        System.out.println("Çarpım = "+islem1.carp());
+
+
+        System.out.println();
+        DortIslem islem2 = new DortIslem(9,11);
+
+
+        System.out.println("Toplam = "+islem2.topla());
+        System.out.println("Çarpım = "+islem2.carp());
+
+
+    }
+}
---
+++ src/day26/math/MainApp.java
@@ -0,0 +1,15 @@
+package day26.math;
+
+public class MainApp {
+    public static void main(String[] args) {
+        int toplam = Islem.topla(5, 7);
+        System.out.println("toplam = " + toplam);
+
+        System.out.println("Çarpım = " + Islem.carp(5, 7));
+
+        long b1= 5;
+        long b2 = 7;
+        Islem.topla(b1,b2);
+    }
+
+}
