1ca612144b99f95d1ee3b4c38e568ef2736fbd0e
Hasan Özyer <hasanozyer06@gmail.com>
2023-05-19 22:52:20 +0300

    OOP constructer examples and extend example

---
+++ src/day27/Aslan.java
@@ -0,0 +1,5 @@
+package day27;
+
+public class Aslan extends Hayvan {
+
+}
---
+++ src/day27/Hayvan.java
@@ -0,0 +1,43 @@
+package day27;
+
+public class Hayvan {
+    private int yas;
+    private double agirlik;
+    private String isim;
+
+    //private yas değişkenin okunup, çağırıldığı yere döndürülür.
+    public int yasiGetir() {
+        return yas;
+    }
+
+    public void yasiKaydet(int yas) {
+        this.yas = yas;
+    }
+
+    public double agirligiGetir() {
+        return agirlik;
+    }
+
+    public void agirligiKaydet(double agirlik) {
+        this.agirlik = agirlik;
+    }
+
+    public String ismiGetir() {
+        return isim;
+    }
+
+    public void ismiKaydet(String isim) {
+        this.isim = isim;
+    }
+
+
+    void beslen() {
+        System.out.println("Hayvan besleniyor.");
+    }
+
+    void uyu() {
+        System.out.println("Hayvan uyuyor.");
+    }
+
+
+}
