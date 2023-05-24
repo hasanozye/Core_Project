1934cc351cc76c91e93c28ae45ec75df096fa4da
Hasan Özyer <hasanozyer06@gmail.com>
2023-05-24 22:14:05 +0300

    oop practise with boxes topic

---
+++ src/day31/kutular/Kutu.java
@@ -0,0 +1,65 @@
+package day31.kutular;
+
+public class Kutu {
+    private int genislik;
+    private int derinlik;
+    private int yukseklik;
+
+    public Kutu() {
+        this(1, 1, 1);
+    }
+
+    public Kutu(int genislik, int derinlik, int yukseklik) {
+        setGenislik(genislik);
+        setDerinlik(derinlik);
+        setYukseklik(yukseklik);
+    }
+
+    public int getGenislik() {
+        return genislik;
+    }
+
+    public void setGenislik(int genislik) {
+        if (genislik > 0) {
+            this.genislik = genislik;
+
+        } else {
+            this.genislik = 1;
+        }
+    }
+
+    public int getDerinlik() {
+        return derinlik;
+    }
+
+    public void setDerinlik(int derinlik) {
+        if (derinlik > 0) {
+            this.derinlik = derinlik;
+
+        } else {
+            this.derinlik = 1;
+        }
+    }
+
+    public int getYukseklik() {
+        return yukseklik;
+    }
+
+    public void setYukseklik(int yukseklik) {
+        if (yukseklik > 0) {
+            this.yukseklik =yukseklik;
+
+        } else {
+            this.yukseklik = 1;
+        }
+    }
+
+    public int getHacim() {
+        return genislik * derinlik * yukseklik;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Kutu : [%d x %d x %d]", genislik, derinlik, yukseklik);
+    }
+}
