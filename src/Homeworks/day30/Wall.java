ba563226a1c363172ea67a0976b8d7a639e17f4c
Hasan Özyer <hasanozyer06@gmail.com>
2023-05-30 20:22:37 +0300

    another oop task to get better understanding of getter setters and encapsulation

---
+++ src/Homeworks/day30/Wall.java
@@ -0,0 +1,46 @@
+package Homeworks.day30;
+
+public class Wall {
+
+    private double width;
+    private double height;
+
+    public Wall() {
+    }
+
+    public Wall(double width, double height) {
+        setWidth(width);
+        setHeight(height);
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        if (width < 0) {
+            this.width = 0;
+        } else {
+            this.width = width;
+        }
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        if (height < 0) {
+            this.height = 0;
+        } else {
+            this.height = height;
+        }
+    }
+
+
+    public double getArea(){
+        return height * width;
+    }
+
+
+}
