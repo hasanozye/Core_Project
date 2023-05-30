2d949958f4bb2854a937ad7db1a25480d3004100
Hasan Özyer <hasanozyer06@gmail.com>
2023-05-30 20:04:55 +0300

    simple oop task to get better understanding

--- src/Homeworks/day28/House.java
+++ src/Homeworks/day28/House.java
@@ -3,9 +3,49 @@ package Homeworks.day28;
 public class House {
     private int bedroom;
     private int bathroom;
-    private String[] heating ={"Gas","Electric"};
+    private String heating;
     private boolean cooling;
     private boolean hasPool;
+
+    public int getBedroom() {
+        return bedroom;
+    }
+
+    public void setBedroom(int bedroom) {
+        this.bedroom = bedroom;
+    }
+
+    public int getBathroom() {
+        return bathroom;
+    }
+
+    public void setBathroom(int bathroom) {
+        this.bathroom = bathroom;
+    }
+
+    public String getHeating() {
+        return heating;
+    }
+
+    public void setHeating(String heating) {
+        this.heating = heating;
+    }
+
+    public boolean isCooling() {
+        return cooling;
+    }
+
+    public void setCooling(boolean cooling) {
+        this.cooling = cooling;
+    }
+
+    public boolean isHasPool() {
+        return hasPool;
+    }
+
+    public void setHasPool(boolean hasPool) {
+        this.hasPool = hasPool;
+    }
 
     public void heat(){
         System.out.println("The house is heated.");
@@ -23,7 +63,7 @@ public class House {
 
     }
 
-    public House(int bedroom, int bathroom, String[] heating) {
+    public House(int bedroom, int bathroom, String heating) {
         this.bedroom = bedroom;
         this.bathroom = bathroom;
         this.heating = heating;
--- src/Homeworks/day28/MainApp.java
+++ src/Homeworks/day28/MainApp.java
@@ -3,11 +3,13 @@ package Homeworks.day28;
 public class MainApp {
     public static void main(String[] args) {
         House house1 = new House();
-        House house2 = new House(3,1,new String[]{"xx","xxa"});
+        House house2 = new House(3,1,"Gas");
 
         house1.heat();
         house1.runCoolingSystem();
+        System.out.println();
 
+        house2.setCooling(true);
         house2.heat();
         house2.runCoolingSystem();
 
