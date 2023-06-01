c5bff578f80bfd0caef1a8e4d35dcda2beb7f26c
Hasan Özyer <hasanozyer06@gmail.com>
2023-06-01 23:32:10 +0300

    Task about char arrays and to string methods in Strings

---
+++ src/day40/MainApp5.java
@@ -0,0 +1,36 @@
+package day40;
+
+public class MainApp5 {
+    public static void main(String[] args) {
+//        StringBuilder, StringBuffer
+        StringBuilder sb = new StringBuilder("Merhaba!");
+
+        sb.append("Nasılsın?");
+        sb.append(5);
+
+        sb.insert(8,"Kardeş.");
+
+//        sb.reverse();
+
+        System.out.println(sb);
+
+        sb.delete(8,15);
+        System.out.println(sb);
+
+        System.out.println(new StringBuilder("Ey edip adanada pide ye").reverse());
+
+
+        System.out.println("sb = " + sb);
+
+        System.out.println(new StringBuilder()
+                .append("Merhaba")
+                .insert(0,"Ahmet")
+                .reverse());
+
+
+
+
+
+
+    }
+}
