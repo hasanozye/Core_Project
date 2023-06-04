84bdda6c83d9f76b6fa45a0b01140b3d2073181d
Hasan Özyer <hasanozyer06@gmail.com>
2023-06-04 16:12:22 +0300

    Furniture mall software project interfaces

---
+++ src/Homeworks/day43/Daire.java
@@ -0,0 +1,8 @@
+package Homeworks.day43;
+
+public interface Daire {
+    void setYaricap(double yaricap);
+    double getYaricap();
+    double alanHesapla();
+    double hacimHesapla();
+}
---
+++ src/Homeworks/day43/Dortgen.java
@@ -0,0 +1,12 @@
+package Homeworks.day43;
+
+public interface Dortgen {
+    void setGenislik(double genislik );
+    double getGenislik();
+    void setDerinlik(double derinlik);
+    double getDerinlik();
+
+    double alanHesapla();
+
+    double hacimHesapla();
+}
