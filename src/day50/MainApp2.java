c7b87a4fd9501e110253b75f89ec25cbdad6e495
Hasan Özyer <hasanozyer06@gmail.com>
2023-06-17 09:19:10 +0300

    I learned about ArrayLists and its methods

---
+++ src/day50/MainApp2.java
@@ -0,0 +1,18 @@
+package day50;
+
+import java.util.ArrayList;
+
+public class MainApp2 {
+    public static void main(String[] args) {
+
+        ArrayList<String> liste = new ArrayList<>();
+        liste.add("Ayşe");
+        liste.add("Şule");
+        liste.add("Betül");
+
+        System.out.println(liste.get(0).toUpperCase());
+
+
+
+    }
+}
