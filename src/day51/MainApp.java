f24537d8c68fb35f725d739643f9ba332af28516
Hasan Özyer <hasanozyer06@gmail.com>
2023-06-19 19:47:20 +0300

    mainapp to execute the app

---
+++ src/day51/MainApp.java
@@ -0,0 +1,56 @@
+package day51;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class MainApp {
+    public static void main(String[] args) {
+        int[] dizi = {1, 2, 5, 7};
+        for (var item : dizi) {
+
+        }
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(5);
+        numbers.add(7);
+
+        for (var item : numbers) {
+
+        }
+
+
+        ShowRoom showRoom = new ShowRoom();
+        System.out.println("Capacity : " + showRoom.capacity());
+        System.out.println("Size : " + showRoom.size());
+
+        showRoom.add(new Car(1, "Araba 1"));
+        showRoom.add(new Car(2, "Araba 2"));
+        showRoom.add(new Car(3, "Araba 3"));
+
+
+        System.out.println("Capacity : " + showRoom.capacity());
+        System.out.println("Size : " + showRoom.size());
+/*
+        System.out.println(showRoom.get(0));
+        System.out.println(showRoom.get(1));
+        System.out.println(showRoom.get(2));
+        System.out.println(showRoom.get(10));*/
+
+        /*for (var item : showRoom) {
+            System.out.println(item);
+        }*/
+
+        /*Iterator<Car> iterator = showRoom.iterator();
+        while (iterator.hasNext()) {
+
+            System.out.println(iterator.next());
+        }*/
+
+        /*showRoom.forEach(item -> System.out.println(item));*/
+
+        showRoom.forEach(System.out::println);
+
+    }
+}
