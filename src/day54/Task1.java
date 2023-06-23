283093d1b35457ad8e19f5fdb3ba333408f40f3d
Hasan Özyer <hasanozyer06@gmail.com>
2023-06-23 09:49:11 +0300

    A task about numbers and sorting method in java

---
+++ src/day54/Task1.java
@@ -0,0 +1,16 @@
+package day54;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task1 {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i) % 2 != 0) {
+                int silinecekSayi = numbers.get(i);
+                numbers.remove(i);
+            }
+        }
+    }
+}
