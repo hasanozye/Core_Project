f76076cc92474422a526dc5059f0cfda49d52ec7
Mete <hasanozyer06@gmail.com>
2023-06-26 11:10:29 +0300

    Leopar Sub-Class to practice polymorphsim

---
+++ src/Homeworks/polypoly/Leopar.java
@@ -0,0 +1,10 @@
+package Homeworks.polypoly;
+
+public class Leopar extends Animal {
+
+    @Override
+    public void eat(){
+        System.out.println("the Leopard is enjoying a meaty meatty meal.");
+    }
+
+}
