45055898b44e933378fc336f9337152e57bfed36
Mete <hasanozyer06@gmail.com>
2023-06-26 11:09:52 +0300

    Koala Sub-Class to practice polymorphsim

---
+++ src/Homeworks/polypoly/Koala.java
@@ -0,0 +1,8 @@
+package Homeworks.polypoly;
+
+public class Koala extends Animal{
+    @Override
+    public void eat() {
+        System.out.println(" the Koala is taking a nap while laying on the tree.");
+    }
+}
