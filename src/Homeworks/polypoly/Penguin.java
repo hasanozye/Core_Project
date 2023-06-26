c2bd09294dfa74a7d1809d3070ec4148cef57888
Mete <hasanozyer06@gmail.com>
2023-06-26 11:10:50 +0300

    Penguin Sub-Class to practice polymorphsim

---
+++ src/Homeworks/polypoly/Penguin.java
@@ -0,0 +1,9 @@
+package Homeworks.polypoly;
+
+public class Penguin extends Animal {
+
+    @Override
+    public void eat() {
+        System.out.println("The penguins killed the humans and ate their livers.");
+    }
+}
