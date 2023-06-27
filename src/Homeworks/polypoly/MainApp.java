d68536c20e92842e35841885d7dff0754bb72589
Mete <hasanozyer06@gmail.com>
2023-06-27 10:58:18 +0300

    Polymorphism mainapp to practice polymorphsim

---
+++ src/Homeworks/polypoly/MainApp.java
@@ -0,0 +1,16 @@
+package Homeworks.polypoly;
+
+public class MainApp {
+    public static void main(String[] args) {
+        Animal leopar = new Leopar();
+        Animal koala = new Koala();
+        Animal penguin = new Penguin();
+
+        leopar.eat();
+        koala.eat();
+        penguin.eat();
+
+
+
+    }
+}
