43b3b132d71381f7b4787af7fa4e41fcd20aacfd
Mete <hasanozyer06@gmail.com>
2023-06-28 07:41:33 +0300

    Lion class initialized and executed in the mainapp

---
+++ src/day60/MainApp3.java
@@ -0,0 +1,11 @@
+package day60;
+
+public class MainApp3 {
+    public static void main(String[] args) {
+        Aslan as1 = new Aslan();
+        /*Thread th1 = new Thread(as1);
+        th1.start();*/
+        as1.ilerle();
+
+    }
+}
